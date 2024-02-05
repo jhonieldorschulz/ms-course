0f36dd6d416d212419c05e5a0d32b60556f95855
jhonieldorschulz <jhonieldorschulz@gmail.com>
2024-02-05 16:28:53 -0300

    Commit inicial dos projetos

---
+++ hr-eureka-server2/src/test/java/com/devsuperior/hreurekaserver2/HrEurekaServer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hreurekaserver2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServer2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
